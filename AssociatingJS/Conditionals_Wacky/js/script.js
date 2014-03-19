af3aa8ee65c5c3212a52c284d8dd6dc6c5f5cf13
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-19 13:50:32 -0400

    Conditionals_Wacky: finished conditional and tested Logical operator &&

--- AssociatingJS/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals_Wacky/js/script.js
@@ -18,17 +18,27 @@ var amountFirstWave= firstWave *10000;
 if (amountFirstWave > 30000){
     var battle1= amountFirstWave-30000; alert("They enemy has attacked your " + firstWave + " legion(s). They killed 30,000 of your men. But you reached the city wall!");
 }else{
-    var battle1= amountFirstWave-10000; alert("The enemy has attacked your " + firstWave + " legions(s). Destroying all your men. You did not reach the city wall.");
+    var battle1= amountFirstWave-amountFirstWave; alert("The enemy has attacked your " + firstWave + " legions(s). Destroying all your men. You did not reach the city wall.");
 }
 
 
-var dragonDefense= prompt("The first wave of attack is over. The enemy seeing an opportunity and sends out their own 2 dragons to your remaining army. \nHow many of your 2 dragons do you send out to battle their dragons? (1-2) ")
+var dragonDefense= prompt("The first wave of attack is over. The enemy seeing an opportunity and sends out their own 2 dragons to your remaining army. \nHow many of your 2 dragons do you send out to battle their dragons. (1 or 2)");
 
 if (dragonDefense == 2){
     alert("The dragon battle was epic, all your dragons you sent out died in combat,as well as the enemy's dragons. You have no dragons left in your army.");
 
 }else{
     alert("The dragon battle was epic, all your dragons you sent out died in combat, leaving you with the 1 dragon left to attack the enemy with.")
+
+}
+
+
+
+
+if  (dragonDefense==1 && battle1 >= 10000){
+    console.log("Since your army survived the first attack wave and you have a dragon remaining, You are strong enough to storm the city gates. VICTORY IS YOURS");
+}else{
+    console.log("Your first attack was too weak, and you have no dragons, the enemy can overpower your attack, YOU HAVE LOST.");
 }
 
 
@@ -36,8 +46,16 @@ if (dragonDefense == 2){
 
 
 
-console.log(dragonDefense);
+alert("The battle is over, dragons and man fought hard, the results in the Console. ");
 
-console.log("After the first wave of your attack, you have " +battle1+ " troops left in your legions sent out.");          //this is used to test expressions in the console.
+
+
+
+
+
+
+
+
+//console.log("After the first wave of your attack, you have " +battle1+ " troops left in your legions sent out.");          //this is used to test expressions in the console.
 
 
