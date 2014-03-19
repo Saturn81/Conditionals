5a654914d0f9e89679d99a4967f9e6aa8d10afe1
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-19 00:33:22 -0400

    Conditionals_Wacky, add first conditional,

--- AssociatingJS/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals_Wacky/js/script.js
@@ -9,7 +9,15 @@ alert("You are taking back your 9 kingdoms. This battle is with swords, and drag
 
 alert("You approach the major city in Easterous. Your going to attack it. you have at your command: \n5 legions (each legion is 10,000 men)\n2 dragons, (powerful monsters)\n2 squads of archers  (each squad is 2,000 men)");
 
-prompt("FIRST WAVE OF ATTACK: \nHow many legions do you send out? (1-5)");
+var firstWave= prompt("FIRST WAVE OF ATTACK: \nHow many legions do you send out? (1-5)")*10000;
+
+
+
+if (firstWave > 30000){
+    console.log("we are had defeated them in the first attack");
+}else{
+    console.log("The enemy has overwhelmed us.");
+}
 
 
 
@@ -17,8 +25,6 @@ prompt("FIRST WAVE OF ATTACK: \nHow many legions do you send out? (1-5)");
 
 
 
-
-
-console.log(prompt);
+//console.log(firstWave);
 
 console.log("tester")          //this is used to test expressions in the console.
