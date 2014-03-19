91fb7518123faa70d72c6535fc20d2a3f918fd0a
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-19 00:05:09 -0400

    Conditionals_Wacky: explain formula, and get up first alerts

--- AssociatingJS/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals_Wacky/js/script.js
@@ -4,4 +4,21 @@
  */
 
 
-alert("Test of Wacky Wacky Conditionals");
+alert("You are taking back your 9 kingdoms. This battle is with swords, and dragons, and is fantasy and very copyrighted by other people. So names have been changed. \n\nYou command the troops and decide how many soldiers fight at a given time.");
+
+
+alert("You approach the major city in Easterous. Your going to attack it. you have at your command: \n5 legions (each legion is 10,000 men)\n2 dragons, (powerful monsters)\n2 squads of archers  (each squad is 2,000 men)");
+
+prompt("FIRST WAVE OF ATTACK: \nHow many legions do you send out? (1-5)");
+
+
+
+
+
+
+
+
+
+console.log(prompt);
+
+console.log("tester")          //this is used to test expressions in the console.
