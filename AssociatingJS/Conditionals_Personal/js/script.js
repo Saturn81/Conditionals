497fd1dc48082400450538829c10597d1300866a
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-20 13:00:17 -0400

    conditionals_Personal: tested formula, first conditional works

--- AssociatingJS/Conditionals_Personal/js/script.js
+++ AssociatingJS/Conditionals_Personal/js/script.js
@@ -5,21 +5,36 @@
  */
 
 
-alert("I want to build a fire pit in the backyard. I need to inventory the necessary tools to build it and see if I can afford building the whole thing.");
+alert("I want to build a fire pit in the backyard. I need to see if I have enough money for the tools and the material to make the fire pit.");
 
 
 var limit =300
 
 var  mallet	= prompt("Do you have a mallet?","yes or no" );
-var  tampler= prompt("Do you have a hand tampler?","yes or no");
-var shovel = prompt("Do you have a shovel>","yes or no");
+var  tampler= prompt("Do you have a hand-tampler?","yes or no");
+var shovel = prompt("Do you have a shovel?","yes or no");
 
 
-if (mallet=yes) {
-   var limit= 300- 16.00
+if (mallet=="no") {
+   var limit= 300- 16;
+}else{
+    var limit=300;
+}
+
+if (tampler=="no"){
+    var limit2= limit-65;
+}else{
+    var limit2=limit;
+}
+
+if (shovel=="no"){
+    var limit=limit2-30;
+}else{
+    var limit=limit2;
 }
 
 
+alert("With inventorying the tools needed, your budget is now $"+limit+" for the remaining items.");
 
 
 
