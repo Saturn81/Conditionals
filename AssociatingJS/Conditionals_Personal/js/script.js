017e2747bcbdebf15cdb129cb0ca4d443b6b6612
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-20 12:28:54 -0400

    Conditionals_Personal: add more prompts and 1st part of conditional

--- AssociatingJS/Conditionals_Personal/js/script.js
+++ AssociatingJS/Conditionals_Personal/js/script.js
@@ -5,7 +5,23 @@
  */
 
 
-alert("I want to build a fire pit in the backyard. I need to inventory the nessarry tools to build it and see if I can afford building the whole thing.");
+alert("I want to build a fire pit in the backyard. I need to inventory the necessary tools to build it and see if I can afford building the whole thing.");
 
 
-console.log("test");
+var limit =300
+
+var  mallet	= prompt("Do you have a mallet?","yes or no" );
+var  tampler= prompt("Do you have a hand tampler?","yes or no");
+var shovel = prompt("Do you have a shovel>","yes or no");
+
+
+if (mallet=yes) {
+   var limit= 300- 16.00
+}
+
+
+
+
+
+    console.log(mallet);
+console.log(limit);
