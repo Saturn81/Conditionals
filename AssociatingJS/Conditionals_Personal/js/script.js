19ff67c3ee99809de7d459cb53322634b1f8e891
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-20 14:35:53 -0400

    Conditionals_Personal: add 2nd part to conditionals

--- AssociatingJS/Conditionals_Personal/js/script.js
+++ AssociatingJS/Conditionals_Personal/js/script.js
@@ -5,7 +5,7 @@
  */
 
 
-alert("I want to build a fire pit in the backyard. I need to see if I have enough money for the tools and the material to make the fire pit.");
+alert("I want to build a stone walled fire pit in the backyard. I need to see if I have enough money for the tools and the material to make the fire pit.");
 
 
 var limit =300
@@ -33,10 +33,27 @@ if (shovel=="no"){
     var limit=limit2;
 }
 
+alert("With inventorying the essential tools needed, your have now $"+limit+" for the remaining building materials, like stone bricks and metal fire pit frame.");
 
-alert("With inventorying the tools needed, your budget is now $"+limit+" for the remaining items.");
+limit2=limit-125
+
+alert("The metal fire pit frame is $125.00, so that amount will subtracted from your remaining money to leave you with $"+(limit2)+".00 left in your budget for stone.");
+
+
+var stoneFancy=128
+
+var stoneRegular= 85
 
 
 
-    console.log(mallet);
-console.log(limit);
+
+
+
+   // console.log(mallet);
+console.log(limit2);
+
+
+
+//console.log("You are below your budget, you can start to build the fire pit. Good Luck!");
+
+//console.log("You don't have enough money in the budget, borrow some tools or get cheaper stones.");
